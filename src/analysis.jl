70a28fdbeb56137b0d4389e0920dc24281e3a20c
Tamas Nagy <tamas@tamasnagy.com>
2018-01-05 18:21:57 -0800

    fix accidental indentation

--- src/analysis.jl
+++ src/analysis.jl
@@ -1,7 +1,7 @@
 """
     extract_peak_interval(run; α, n)
 
-    Takes a CoulterCounterRun `run` and fits a kernel density estimate to smooth
+Takes a CoulterCounterRun `run` and fits a kernel density estimate to smooth
 out binning effects by the machine and then finds the largest peak. It returns
 the diameter corresponding to the location of this peak as well as the max and
 min diameters of the confidence interval defined by `α`. This interval is
