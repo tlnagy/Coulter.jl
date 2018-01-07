8be6c1f74a4800e05b072bf3ada1f8fd847555d8
Tamas Nagy <tamas@tamasnagy.com>
2018-01-06 22:48:49 -0800

    add loading tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using Coulter
 using Base.Test
 
-# write your own tests here
-@test volume(0.0) == 0.0
-@test volume(10.0) ≈ 523.59878
+@testset "Loading" begin
+    data = loadZ2("testdata/b_0_1.=#Z2", "blank")
+
+    @test data.timepoint == DateTime(2017, 12, 22, 17, 46, 58)
+    @test all(data.binheights .== 0.0)
+end
+
+@testset "Misc" begin
+    @test volume(0.0) == 0.0
+    @test volume(10.0) ≈ 523.59878
+end
