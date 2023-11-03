501f735f28c813f7c525cbe988041a82cd5ed4e0
Tamas Nagy <tamas@tamasnagy.com>
2023-11-03 16:08:51 -0700

    add support for streams (#17)
    * add support for streams
    
    This is needed for reading from compressed files
    
    * bump version

--- src/Coulter.jl
+++ src/Coulter.jl
@@ -40,44 +40,48 @@ module Coulter
     deepcopy(a::CoulterCounterRun) = CoulterCounterRun([deepcopy(getfield(a, field)) for field in fieldnames(CoulterCounterRun)]...)
 
     """
-    loadZ2(filename::String, sample::String)
+    loadZ2(filename, sample)
 
-    Loads `filename` and assigns it a sample, returns a
+    Loads `filename` and assigns it to a `sample` group, returns a
     `CoulterCounterRun` object
     """
-    function loadZ2(filepath::String, sample::String; yvariable=:count)
+    function loadZ2(filepath, sample; yvariable=:count)
         open(filepath) do s
-            # split windows newlines if present
-            filebody = replace(read(s, String), "\r\n"=>"\n")
-            # extract start time and date from body
-            datetime = match(r"^StartTime= \d*\s*(?<time>\d*:\d*:\d*)\s*(?<date>\d*\s\w{3}\s\d{4})$"m, filebody)
-            timepoint = DateTime("$(datetime[:date]) $(datetime[:time])", "dd uuu yyy HH:MM:SS")
-
-            params = Dict{String, Any}()
-            params["Current"] = parse(Float64, match(r"^Cur=([+-]?[0-9]*[.]?[0-9]+)$"m, filebody)[1])
-            params["Pre-Amp Gain"] = parse(Float64, match(r"^PAGn=([+-]?[0-9]*[.]?[0-9]+)$"m, filebody)[1])
-            params["Volume metered"] = parse(Float64, match(r"^Vol=([+-]?[0-9]*[.]?[0-9]+)$"m, filebody)[1])
-
-            # extract data
-            matcheddata = match(r"^\[#Bindiam\]\n(?<binvols>.*?)\n^\[Binunits\].*?\[#Binheight\]\n(?<binheight>.*?)\n^\[end\]"sm, filebody)
-            binlims = [volume(parse(Float64, x)) for x in split(matcheddata[:binvols], "\n ")]
-            # the Coulter software takes the mean the upper and lower limits to
-            # compute the volume of everything that falls in the bin
-            binvols = [(binlims[i] + binlims[i+1])/2 for i in 1:length(binlims)-1]
-            binheights = Int[parse(Int, x) for x in split(matcheddata[:binheight], "\n ")]
-            pop!(binheights) # remove the last extraneous value
-
-            # unbin data, i.e. the inverse of the hist function
-            data = repvec(binvols, binheights)
-
-            if yvariable == :volume
-                binheights = binvols .* binheights
-            end
-
-            CoulterCounterRun(basename(filepath), sample, timepoint, nothing,
-                              binlims, binvols, binheights, yvariable, data,
-                              nothing, Float64[], params)
+            loadZ2(s, filepath, sample; yvariable = yvariable)
         end
+    end
+
+    function loadZ2(io::S, filepath, sample; yvariable=:count) where {S <: IO}
+        # split windows newlines if present
+        filebody = replace(read(io, String), "\r\n"=>"\n")
+        # extract start time and date from body
+        datetime = match(r"^StartTime= \d*\s*(?<time>\d*:\d*:\d*)\s*(?<date>\d*\s\w{3}\s\d{4})$"m, filebody)
+        timepoint = DateTime("$(datetime[:date]) $(datetime[:time])", "dd uuu yyy HH:MM:SS")
+
+        params = Dict{String, Any}()
+        params["Current"] = parse(Float64, match(r"^Cur=([+-]?[0-9]*[.]?[0-9]+)$"m, filebody)[1])
+        params["Pre-Amp Gain"] = parse(Float64, match(r"^PAGn=([+-]?[0-9]*[.]?[0-9]+)$"m, filebody)[1])
+        params["Volume metered"] = parse(Float64, match(r"^Vol=([+-]?[0-9]*[.]?[0-9]+)$"m, filebody)[1])
+
+        # extract data
+        matcheddata = match(r"^\[#Bindiam\]\n(?<binvols>.*?)\n^\[Binunits\].*?\[#Binheight\]\n(?<binheight>.*?)\n^\[end\]"sm, filebody)
+        binlims = [volume(parse(Float64, x)) for x in split(matcheddata[:binvols], "\n ")]
+        # the Coulter software takes the mean the upper and lower limits to
+        # compute the volume of everything that falls in the bin
+        binvols = [(binlims[i] + binlims[i+1])/2 for i in 1:length(binlims)-1]
+        binheights = Int[parse(Int, x) for x in split(matcheddata[:binheight], "\n ")]
+        pop!(binheights) # remove the last extraneous value
+
+        # unbin data, i.e. the inverse of the hist function
+        data = repvec(binvols, binheights)
+
+        if yvariable == :volume
+            binheights = binvols .* binheights
+        end
+
+        CoulterCounterRun(basename(filepath), sample, timepoint, nothing,
+                            binlims, binvols, binheights, yvariable, data,
+                            nothing, Float64[], params)
     end
 
     loadZ2(filepath::String) = loadZ2(filepath, "N/A")
